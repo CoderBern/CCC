5830bce7b27d44d06d2512001fa25ad57115c348
CoderBern <88506415+CoderBern@users.noreply.github.com>
2021-08-05 17:20:07 -0400

    Create ccc04j2.cpp

---
+++ ccc04j2.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int x,y;
+    cin>>x>>y;
+    for(int i=x;i<=y;i=i+60){
+        cout<<"All positions change in year "<<i<<"\n";
+    }
+}
