cb649d414d74c9ec6b6ca08c27fcdce0c6101004
CoderBern <88506415+CoderBern@users.noreply.github.com>
2021-08-06 11:40:16 -0400

    Create ccc11j1.cpp

---
+++ ccc11j1.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int a,b;
+    cin>>a>>b;
+    if(a>=3 and b<=4) cout<<"TroyMartian\n";
+    if(a<=6 and b>=2) cout<<"VladSaturnian\n";
+    if(a<=2 and b<=3) cout<<"GraemeMercurian\n";
+
+}
