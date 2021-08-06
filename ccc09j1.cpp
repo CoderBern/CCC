a4353e32f6968dfc2f8875b78e6cde5f12a5f583
CoderBern <88506415+CoderBern@users.noreply.github.com>
2021-08-06 11:24:01 -0400

    Create ccc09j1.cpp

---
+++ ccc09j1.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int a,b,c,sum;
+    sum=9*1+7*3+8*1+0*3+9*1+2*3+1*1+4*3+1*1+8*3;
+    cin>>a>>b>>c;
+    sum+=a*1+b*3+c*1;
+    cout<<"The 1-3-sum is "<<sum<<"\n";
+
+}
