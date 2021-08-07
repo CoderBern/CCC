2ea2fe5fc6cc0e2c8ead55171119f5249ab85696
CoderBern <88506415+CoderBern@users.noreply.github.com>
2021-08-07 00:05:23 -0400

    Update ccc01s2.cpp

--- ccc01s2.cpp
+++ ccc01s2.cpp
@@ -3,19 +3,7 @@
 using namespace std;
 
 int arr[10][10];
-
-int rowSum(int i){
-    int sum=0;
-    for(int j=0;j<10;j++) sum+=arr[i][j];
-    return sum;
-}
-
-int colSum(int i){
-    int sum=0;
-    for(int j=0;j<10;j++) sum+=arr[j][i];
-    return sum;
-}
-
+// format requirement is not strict
 int main() {
     int a,b;
     cin>>a>>b;
@@ -48,37 +36,15 @@ int main() {
 
     }
     
-    flag=true;
-    int row_start, row_end=9, col_start, col_end=9;
-    for(int i=0;i<10;i++){
-        if(flag and rowSum(i)!=0){
-            flag=false;
-            row_start=i;
-        } else if(!flag and rowSum(i)==0){
-            flag=true;
-            row_end=i-1;
-        }
-    }
-    
-    flag=true;
-    for(int i=0;i<10;i++){
-        if(flag and colSum(i)!=0){
-            flag=false;
-            col_start=i;
-        } else if(!flag and colSum(i)==0){
-            flag=true;
-            col_end=i-1;
-        }
-    }
-    
+   
+   
     int i,j;
-    for(i=row_start; i<=row_end;i++){
-        for(j=col_start; j<=col_end-1; j++){
+    for(i=0; i<10;i++){
+        for(j=0; j<10; j++){
             if(arr[i][j]==0) cout<<"   ";
             else cout<<arr[i][j]<<" ";
         }
-        if(arr[i][j]==0) cout<<"  \n";
-        else cout<<arr[i][j]<<"\n";
+        cout<<"\n";
     }
    
 }
